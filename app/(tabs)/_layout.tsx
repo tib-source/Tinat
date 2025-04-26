e58ee3ada9968c7ce43bc3b266a18de112ff75f4
Tibebe Demissie <tib.demissie@gmail.com>
2025-04-27 00:05:24 +0100

    feat: add tab bar icons for Home, Bible, and Calendar screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,16 @@
 import { Tabs } from "expo-router";
 import { useTranslation } from "react-i18next";
 import { ThemeToggle } from "~/components/ThemeToggle";
+import { CalendarDays, HomeIcon } from "~/lib/icons/Navigation"
+import { BookOpen } from "~/lib/icons/Book"
 import "~/translation"
 
 export default function TabLayout() {
     const { t } = useTranslation();
 
     return <Tabs>
-        <Tabs.Screen name="index" options={{title: "Home", headerTitle: t("home.greeting", {"name": "Tibebe"}), headerRight: () => <ThemeToggle />,}} />
-        <Tabs.Screen name='bible' options={{ title: "Bible", headerShown: false }} />
-        <Tabs.Screen name="calendar" options={{ title: "Calendar", headerShown: false}} />
-
-
+        <Tabs.Screen name="index" options={{title: "Home", headerTitle: t("home.greeting", {"name": "Tibebe"}), headerRight: () => <ThemeToggle />, tabBarIcon : ({ color }) => <HomeIcon color={color}/>}} />
+        <Tabs.Screen name='bible' options={{ title: "Bible", headerShown: false, tabBarIcon : ({ color }) => <BookOpen color={color}/> }} />
+        <Tabs.Screen name="calendar" options={{ title: "Calendar", headerShown: false, tabBarIcon : ({ color }) => <CalendarDays color={color}/>}} />
     </Tabs>
 }
---
+++ lib/icons/Navigation.tsx
@@ -0,0 +1,5 @@
+import { HomeIcon , CalendarDays} from "lucide-react-native";
+import { iconWithClassName } from "./iconWithClassName";
+iconWithClassName(HomeIcon)
+iconWithClassName(CalendarDays)
+export { HomeIcon, CalendarDays }; 
