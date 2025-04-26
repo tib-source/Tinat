d4b035170f340152f92747b7cf36d655d8b3262e
Tibebe Demissie <tib.demissie@gmail.com>
2025-04-26 12:58:38 +0100

    feat: implement Bible content display

--- app/(tabs)/bible/index.tsx
+++ app/(tabs)/bible/index.tsx
@@ -1,31 +1,74 @@
-import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+import { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Text, View } from "react-native";
-import { useEffect, useState } from "react";
+import { FlatList, NativeSyntheticEvent, Text, TextInputChangeEventData, TouchableOpacity, View } from "react-native";
+import { Input } from "~/components/ui/input";
+import bibleListData from "~/assets/data/bible_books.json";
+import { Card } from "~/components/ui/card";
+import { Progress } from "~/components/ui/progress";
+interface Book {
+    bookNumber: number;
+    book_en: string;
+    book_am: string;
+    chapters: number;
+    chapters_read: number;
+}
 
-function BibleContent() {
-
-  const { t } = useTranslation();
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-2xl font-bold text-foreground">{t('home.greeting', {"name": "Bible"})}</Text>
-    </View>
-  );
+interface BibleList {
+    books: Book[];
 }
 
 export default function Index() {
-  return (
-    <SQLiteProvider 
-      databaseName="bible.sqlite"
-    >
-      <BibleContent />
-    </SQLiteProvider>
-  );
+
+    const [search, setSearch] = useState("");
+    const { t } = useTranslation();
+    const bibleList = useMemo<BibleList>(() => {
+        return bibleListData as BibleList;
+    }, []);
+
+    const [searchResults, setSearchResults] = useState<Book[]>(bibleList.books);
+    const handleSearch = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+        console.log(e.nativeEvent.text);
+    };
+
+
+    return (
+        <View className="flex-1 p-2">
+            <Input className="rounded-2xl mb-5" placeholder="Search Books..." onChange={handleSearch} />
+            <View className="">
+                <FlatList
+                    data={searchResults}
+                    keyExtractor={(item) => item.bookNumber.toString()}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity className="flex-0.65" onPress={() => console.log(item)}>
+                            <Card className="w-50 p-4 mb-2 bg-background rounded-2xl">
+                                <View className="flex-row items-center justify-between">
+                                    <Text className="text-lg font-bold">{item.book_am}</Text>
+                                    <Text className="text-sm text-muted-foreground">{item.chapters} chapters</Text>
+                                </View>
+                                <View className="flex-row items-center justify-between mt-2">
+                                    <Text className="text-sm text-muted-foreground">Progress</Text>
+                                    <Text className="text-sm text-muted-foreground">{Math.floor((item.chapters_read / item.chapters ) * 100) }%</Text>
+                                </View>
+                                <Progress value={Math.floor((item.chapters_read / item.chapters) * 100)} className="h-2 mt-1" indicatorClassName="bg-primary" />
+                            </Card>
+                        </TouchableOpacity>
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 100 }}
+                    ListEmptyComponent={
+                        <View className="flex-1 items-center justify-center">
+                            <Text className="text-lg text-muted-foreground">{t("bible.no_results")}</Text>
+                        </View>
+                    }
+                    ListHeaderComponent={
+                        <View className="flex-row items-center justify-between mb-2">
+                            <Text className="text-lg font-bold">Chapters</Text>
+                            <Text className="text-sm text-muted-foreground">{bibleList.books.length} books</Text>
+                        </View>
+                    }
+                    />
+            </View>
+        </View>
+    );
 }
--- src/db/seedDatabase.js
+++ src/db/seedDatabase.js
@@ -1,58 +1,75 @@
 const fs = require('fs');
-const Database = require('better-sqlite3');
+// const Database = require('better-sqlite3');
 
 // Load your JSON file
 const bibleData = JSON.parse(fs.readFileSync('./amharic_bible.json', 'utf-8'));
 
-// Create or overwrite the SQLite DB
-const db = new Database('bible.sqlite');
+// // Create or overwrite the SQLite DB
+// const db = new Database('bible.sqlite');
 
-// Drop and create tables
-db.exec(`
-  DROP TABLE IF EXISTS books;
-  DROP TABLE IF EXISTS chapters;
-  DROP TABLE IF EXISTS verses;
+// // Drop and create tables
+// db.exec(`
+//   DROP TABLE IF EXISTS books;
+//   DROP TABLE IF EXISTS chapters;
+//   DROP TABLE IF EXISTS verses;
 
-  CREATE TABLE books (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title_am TEXT,
-    title_en TEXT,
-    order_index INTEGER
-  );
+//   CREATE TABLE books (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     title_am TEXT,
+//     title_en TEXT,
+//     order_index INTEGER
+//   );
 
-  CREATE TABLE chapters (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    book_id INTEGER,
-    chapter_number INTEGER,
-    FOREIGN KEY(book_id) REFERENCES books(id)
-  );
+//   CREATE TABLE chapters (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     book_id INTEGER,
+//     chapter_number INTEGER,
+//     FOREIGN KEY(book_id) REFERENCES books(id)
+//   );
 
-  CREATE TABLE verses (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    chapter_id INTEGER,
-    verse_number INTEGER,
-    text_am TEXT,
-    text_en TEXT,
-    FOREIGN KEY(chapter_id) REFERENCES chapters(id)
-  );
-`);
+//   CREATE TABLE verses (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     chapter_id INTEGER,
+//     verse_number INTEGER,
+//     text_am TEXT,
+//     text_en TEXT,
+//     FOREIGN KEY(chapter_id) REFERENCES chapters(id)
+//   );
+// `);
 
-const insertBook = db.prepare(`INSERT INTO books (title_am, title_en, order_index) VALUES (?, ?, ?)`);
-const insertChapter = db.prepare(`INSERT INTO chapters (book_id, chapter_number) VALUES (?, ?)`);
-const insertVerse = db.prepare(`INSERT INTO verses (chapter_id, verse_number, text_am, text_en) VALUES (?, ?, ?, ?)`);
+// const insertBook = db.prepare(`INSERT INTO books (title_am, title_en, order_index) VALUES (?, ?, ?)`);
+// const insertChapter = db.prepare(`INSERT INTO chapters (book_id, chapter_number) VALUES (?, ?)`);
+// const insertVerse = db.prepare(`INSERT INTO verses (chapter_id, verse_number, text_am, text_en) VALUES (?, ?, ?, ?)`);
+
+// smaller json 
+let minified = {
+  books: [],
+}
+
 
 bibleData.books.forEach((book, bookIndex) => {
-    const bookResult = insertBook.run(book.title, "", bookIndex);
-    const bookId = bookResult.lastInsertRowid;
+    // const bookResult = insertBook.run(book.title, "", bookIndex);
+    // const bookId = bookResult.lastInsertRowid;
   
-    book.chapters.forEach((chapter, chapterIndex) => {
-      const chapterResult = insertChapter.run(bookId, chapterIndex + 1);
-      const chapterId = chapterResult.lastInsertRowid;
-  
-      chapter.verses.forEach((verseText, verseIndex) => {
-        insertVerse.run(chapterId, verseIndex + 1, verseText, ""); // leave English empty if not available
-      });
+    const length = book.chapters.length;
+
+    minified.books.push({
+      bookNumber: bookIndex + 1,
+      book_am: book.title,
+      book_en: book.title,
+      chapters: length,
+      chapters_read: Math.floor(Math.random() * length),
     });
+    // book.chapters.forEach((chapter, chapterIndex) => {
+      // const chapterResult = insertChapter.run(bookId, chapterIndex + 1);
+      // const chapterId = chapterResult.lastInsertRowid;
+
+      // chapter.verses.forEach((verseText, verseIndex) => {
+      //   insertVerse.run(chapterId, verseIndex + 1, verseText, ""); // leave English empty if not available
+      // });
+    // });
   });
   
+
+  console.log(minified);
   console.log("✅ Bible database created with chapters: bible.sqlite");
